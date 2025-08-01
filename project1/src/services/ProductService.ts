4c447c12d1ee5610521ed6d5ad356a67b0319696
Tvaexe <vietanh.vadc.2004@gmail.com>
2025-08-01 11:00:56 +0700

    khớp data vào trang chủ

--- project1/src/services/ProductService.ts
+++ project1/src/services/ProductService.ts
@@ -2,24 +2,12 @@ import { Product, CartItem, Review } from "../types/Product";
 import axiosInstance from "./AxiosCustom";
 
 export async function getProducts(): Promise<Product[]> {
-  const response = await axiosInstance.get<Product[]>("/productInfo");
+  const response = await axiosInstance.get<Product[]>("/products");
   return response.data;
 }
 
-export async function getBestSellers(): Promise<Product[]> {
-  const response = await axiosInstance.get<Product[]>("/bestSellers");
-  return response.data;
-}
 
-export async function getPromotions(): Promise<Product[]> {
-  const response = await axiosInstance.get<Product[]>("/promotions");
-  return response.data;
-}
 
-export async function getNewArrival(): Promise<Product[]> {
-  const response = await axiosInstance.get<Product[]>("/newArrival");
-  return response.data;
-}
 
 export const getProductById = async (id: number): Promise<Product> => {
     try{
